9e3753c33594c6df53506fc4c5c75b816cec18fc
smalllazyboy <2583509733@qq.com>
2025-08-20 17:12:02 +0800

    终端设备的控制--Done

--- fs/file_dev.c
+++ fs/file_dev.c
@@ -11,80 +11,100 @@
 #include <linux/kernel.h>
 #include <asm/segment.h>
 
-#define MIN(a,b) (((a)<(b))?(a):(b))
-#define MAX(a,b) (((a)>(b))?(a):(b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
 
-int file_read(struct m_inode * inode, struct file * filp, char * buf, int count)
+extern long my_flag;
+
+int file_read(struct m_inode *inode, struct file *filp, char *buf, int count)
 {
-	int left,chars,nr;
-	struct buffer_head * bh;
+	int left, chars, nr;
+	struct buffer_head *bh;
 
-	if ((left=count)<=0)
+	if ((left = count) <= 0)
 		return 0;
-	while (left) {
-		if ((nr = bmap(inode,(filp->f_pos)/BLOCK_SIZE))) {
-			if (!(bh=bread(inode->i_dev,nr)))
+	while (left)
+	{
+		if ((nr = bmap(inode, (filp->f_pos) / BLOCK_SIZE)))
+		{
+			if (!(bh = bread(inode->i_dev, nr)))
 				break;
-		} else
+		}
+		else
 			bh = NULL;
 		nr = filp->f_pos % BLOCK_SIZE;
-		chars = MIN( BLOCK_SIZE-nr , left );
+		chars = MIN(BLOCK_SIZE - nr, left);
 		filp->f_pos += chars;
 		left -= chars;
-		if (bh) {
-			char * p = nr + bh->b_data;
-			while (chars-->0)
-				put_fs_byte(*(p++),buf++);
+		if (bh)
+		{
+			char *p = nr + bh->b_data;
+			while (chars-- > 0)
+				put_fs_byte(*(p++), buf++);
 			brelse(bh);
-		} else {
-			while (chars-->0)
-				put_fs_byte(0,buf++);
+		}
+		else
+		{
+			while (chars-- > 0)
+				put_fs_byte(0, buf++);
 		}
 	}
 	inode->i_atime = CURRENT_TIME;
-	return (count-left)?(count-left):-ERROR;
+	return (count - left) ? (count - left) : -ERROR;
 }
 
-int file_write(struct m_inode * inode, struct file * filp, char * buf, int count)
+int file_write(struct m_inode *inode, struct file *filp, char *buf, int count)
 {
 	off_t pos;
-	int block,c;
-	struct buffer_head * bh;
-	char * p;
-	int i=0;
+	int block, c;
+	struct buffer_head *bh;
+	char *p;
+	int i = 0;
 
-/*
- * ok, append may not work when many processes are writing at the same time
- * but so what. That way leads to madness anyway.
- */
+	/*
+	 * ok, append may not work when many processes are writing at the same time
+	 * but so what. That way leads to madness anyway.
+	 */
 	if (filp->f_flags & O_APPEND)
 		pos = inode->i_size;
 	else
 		pos = filp->f_pos;
-	while (i<count) {
-		if (!(block = create_block(inode,pos/BLOCK_SIZE)))
+	while (i < count)
+	{
+		if (!(block = create_block(inode, pos / BLOCK_SIZE)))
 			break;
-		if (!(bh=bread(inode->i_dev,block)))
+		if (!(bh = bread(inode->i_dev, block)))
 			break;
 		c = pos % BLOCK_SIZE;
 		p = c + bh->b_data;
 		bh->b_dirt = 1;
-		c = BLOCK_SIZE-c;
-		if (c > count-i) c = count-i;
+		c = BLOCK_SIZE - c;
+		if (c > count - i)
+			c = count - i;
 		pos += c;
-		if (pos > inode->i_size) {
+		if (pos > inode->i_size)
+		{
 			inode->i_size = pos;
 			inode->i_dirt = 1;
 		}
 		i += c;
-		while (c-->0)
-			*(p++) = get_fs_byte(buf++);
+		while (c-- > 0)
+		{
+			// 如果是数字或者字母，修改向文件输出的内容
+			*(p) = get_fs_byte(buf++);
+			if (my_flag && (*p >= '0' && *p <= '9' || *p >= 'a' && *p <= 'z' || *p >= 'A' && *p <= 'Z'))
+			{
+				*p = '*';
+			}
+			p++;
+		}
 		brelse(bh);
 	}
 	inode->i_mtime = CURRENT_TIME;
-	if (!(filp->f_flags & O_APPEND)) {
+	if (!(filp->f_flags & O_APPEND))
+	{
 		filp->f_pos = pos;
 		inode->i_ctime = CURRENT_TIME;
 	}
-	return (i?i:-1);
+	return (i ? i : -1);
 }
